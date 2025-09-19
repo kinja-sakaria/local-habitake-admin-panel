243633bd783ac13f49666b34ad0bf5a308fb358b
kinjal sakaria <kinjal.s@prometteur.in>
2025-09-19 16:53:36 +0530

    update

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,13 @@
-import MainRoutes from './MainRoutes';
-import LoginRoutes from './LoginRoutes';
-import ErrorRoutes from './ErrorRoutes';
 import { createBrowserRouter } from 'react-router-dom';
+import LoginRoutes from './LoginRoutes';
+import MainRoutes from './MainRoutes';
+import ErrorRoutes from './ErrorRoutes';
 import ProtectedRoute from './ProtectedRoute';
-import PublicRoute from './PublicRoute';
 
 const router = createBrowserRouter([
   {
     ...LoginRoutes,
-    element: <PublicRoute>{LoginRoutes.element}</PublicRoute>,
   },
-  // protected routes
   {
     ...MainRoutes,
     element: <ProtectedRoute>{MainRoutes.element}</ProtectedRoute>,
